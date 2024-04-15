839ca9fb749ebc19064dd6fbe3b10b97ae7c6eaa
RoccoCerro <rcerro02@gmail.com>
2024-04-15 17:42:10 +0200

    Refactor: format

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,30 +1,30 @@
 <template>
-        <li class="col">
-            <div class="name">
-                {{ name }}
-            </div>
-            <div class="image">
-                <img :src="image" alt="">
-            </div>
-        </li>
+  <li class="col">
+    <div class="name">
+      {{ name }}
+    </div>
+    <div class="image">
+      <img :src="image" alt="" />
+    </div>
+  </li>
 </template>
 
 <script>
-    export default {
-        props: {
-            name: "string",
-            image: "string",
-        }
-    }
+export default {
+  props: {
+    name: "string",
+    image: "string",
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@use '../assets/scss/general.scss';
+@use "../assets/scss/general.scss";
 
-.name{
-    font-size: large;
-    font-weight: bold;
-    padding: 10px 0;
-    color: rgba(0, 0, 0, 0.445);
+.name {
+  font-size: large;
+  font-weight: bold;
+  padding: 10px 0;
+  color: rgba(0, 0, 0, 0.445);
 }
 </style>
